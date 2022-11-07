dfb2e0640abe7e220e9162ef0a40c629e5f3f687
alexey <alexeylabs@gmail.com>
2022-11-07 09:08:04 +0300

    added validation to main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from model.hidden import Hidden
 from config import config
+from model.noiser import Noiser
 from train import train
 from utils import get_data_loaders
+from validate import validate
 
 
 def main():
@@ -9,6 +11,13 @@ def main():
     train_loader, val_loader = get_data_loaders(config)
 
     train(hidden, config, train_loader, val_loader)
+
+    for noise in config.noiser.keys():
+        noiser_config = {
+            noise: config.noiser[noise],
+        }
+        hidden.noiser = Noiser(noiser_config)
+        validate(hidden, val_loader, config['message_length'], config['device'])
 
 
 main()
